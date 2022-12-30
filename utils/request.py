9e3a612bdf54f1627eda133e484c723b318f9bc0
Ashik Kumar Billava Padmaiah <Ashik.BillavaPadmaiah@pfizer.com>
2022-12-30 15:24:25 +0530

    Add api request utility module

---
+++ utils/request.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+import requests
+
+
+@dataclass
+class Response:
+    status_code: int
+    text: str
+    as_dict: object
+    headers: dict
+
+
+class APIRequest:
+    def post_request(self, url, payload, headers):
+        response = requests.post(url=url, data=payload, headers=headers)
+        return self.__get_responses(response)
+
+    def get_request(self, url, headers):
+        response = requests.get(url=url, headers=headers)
+        return self.__get_responses(response)
+
+    def put_request(self, url, payload, headers):
+        response = requests.put(url=url, data=payload, headers=headers)
+        return self.__get_responses(response)
+
+    def delete_request(self, url, headers):
+        response = requests.delete(url=url, data=None, headers=headers)
+        return self.__get_responses(response)
+
+    @staticmethod
+    def __get_responses(response):
+        status_code = response.status_code
+        text = response.text
+        try:
+            as_dict = response.json()
+        except ValueError:
+            as_dict = {}
+        headers = response.headers
+        return Response(status_code, text, as_dict, headers)
