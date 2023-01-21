33fb5f5561f37bc3d0e46cd3550d84bd102df8ed
Ashik Kumar Billava Padmaiah <Ashik.BillavaPadmaiah@pfizer.com>
2023-01-21 20:01:41 +0530

    Refactor file_reader.py

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,18 +1,16 @@
 import json
 from pathlib import Path
 
-BASE_PATH = Path.cwd().joinpath("data")
+BASE_PATH = Path.cwd() / "data"
 
 
-def read_file(file_name):
+def read_file(file_name: str) -> dict:
     file_path = get_file_with_json_ext(file_name)
     with open(file_path, mode="r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def get_file_with_json_ext(file_name):
-    return (
-        BASE_PATH.joinpath(file_name)
-        if ".json" in file_name
-        else BASE_PATH.joinpath(f"{file_name}.json")
-    )
+def get_file_with_json_ext(file_name: str) -> Path:
+    if not file_name.endswith(".json"):
+        file_name += ".json"
+    return BASE_PATH / file_name
